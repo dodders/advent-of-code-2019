a3c09c07d54fe933072a88ba7e13709ed562a6e8
georged <6376745+dodders@users.noreply.github.com>
2019-12-01 11:05:17 -0500

    day01

--- day01/day01.py
+++ day01/day01.py
@@ -1,3 +1,21 @@
 import util
 
-print(util.get_input(True, '01'))
+
+def mass_fuel(num):
+    ret = []
+    while num > 0:
+        num = num // 3 - 2
+        if num > 0:
+            ret.append(num)
+    return ret
+
+
+inp = util.get_number_input(True, '01')
+p1 = sum([n//3 - 2 for n in inp])
+print("part1:", p1)
+
+p2 = sum([sum(mass_fuel(num)) for num in inp])
+print("part2: ", p2)
+
+
+
--- util.py
+++ util.py
@@ -5,4 +5,9 @@ def get_input(prod, day):
         fname = 'test.txt'
 
     f = open('day' + day + '/' + fname, 'r')
-    return f.readlines()
+    lines = f.readlines()
+    return [line.strip("\n") for line in lines]
+
+
+def get_number_input(prod, day):
+    return [int(num) for num in get_input(prod, day)]
