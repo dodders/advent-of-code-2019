68e4f8a3ea500cb59d9cd3bf4459e10d0addb4bc
georged <6376745+dodders@users.noreply.github.com>
2019-12-01 11:15:13 -0500

    add recursion for part 2

--- day01/day01.py
+++ day01/day01.py
@@ -1,21 +1,21 @@
 import util
 
-
-def mass_fuel(num):
-    ret = []
-    while num > 0:
-        num = num // 3 - 2
-        if num > 0:
-            ret.append(num)
-    return ret
-
-
 inp = util.get_number_input(True, '01')
+
+
+def mass_fuel(mass):
+    fuel = mass // 3 - 2
+    if fuel <= 0:
+        return mass
+    return fuel + mass_fuel(fuel)
+
+
+# part1: 3270717
 p1 = sum([n//3 - 2 for n in inp])
 print("part1:", p1)
 
-p2 = sum([sum(mass_fuel(num)) for num in inp])
+# part2:  4903193
+p2 = sum([mass_fuel(num) for num in inp])
 print("part2: ", p2)
-
 
 
