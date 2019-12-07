849acfcaa451c9fa8e4abcf5113bb3cbb318f82f
georged <6376745+dodders@users.noreply.github.com>
2019-12-07 10:14:35 -0500

    day06

--- util.py
+++ util.py
@@ -11,3 +11,37 @@ def get_input(prod, day):
 
 def get_number_input(prod, day):
     return [int(num) for num in get_input(prod, day)]
+
+
+class Node():
+    def __init__(self, n, parent=None):
+        self.value = n
+        self.children = []
+        if parent is not None:
+            parent.add_child(self)
+
+    def add_child(self, n):
+        self.children.append(n)
+
+    def child_values(self):
+        return [c.value for c in self.children]
+
+    def contains(self, node_val):
+        # print("%s finding %s" % (self.value, node_val))
+        if len(self.children) == 0:
+            # print("%s not found %s returning 0" % (self.value, node_val))
+            return 0
+        if node_val in [c.value for c in self.children]:
+            # print("%s found %s returning 1" % (self.value, node_val))
+            return 1
+        else:
+            for child in self.children:
+                found = child.contains(node_val)
+                if found > 0:
+                    return found + 1
+            return 0
+
+    def __repr__(self):
+        return '%s ->: %s' % (self.value, self.child_values())
+
+
